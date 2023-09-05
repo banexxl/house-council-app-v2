f7b7fbd2fbae09ba0d720deb6abc82e93675c4fe
banexxl <damjanovic.branislav@gmail.com>
2023-09-06 00:10:47 +0200

    bild fix mozda

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -11,157 +11,157 @@ const STORAGE_KEY = 'users';
 //  This should be replaced with a server call that returns DB persisted data.
 
 const getPersistedUsers = (): User[] => {
-  try {
-    const data = sessionStorage.getItem(STORAGE_KEY);
+          try {
+                    const data = sessionStorage.getItem(STORAGE_KEY);
 
-    if (!data) {
-      return [];
-    }
+                    if (!data) {
+                              return [];
+                    }
 
-    return JSON.parse(data) as User[];
-  } catch (err) {
-    console.error(err);
-    return [];
-  }
+                    return JSON.parse(data) as User[];
+          } catch (err) {
+                    console.error(err);
+                    return [];
+          }
 };
 
 const persistUser = (user: User): void => {
-  try {
-    const users = getPersistedUsers();
-    const data = JSON.stringify([...users, user]);
-    sessionStorage.setItem(STORAGE_KEY, data);
-  } catch (err) {
-    console.error(err);
-  }
+          try {
+                    const users = getPersistedUsers();
+                    const data = JSON.stringify([...users, user]);
+                    sessionStorage.setItem(STORAGE_KEY, data);
+          } catch (err) {
+                    console.error(err);
+          }
 };
 
 type SignInRequest = {
-  email: string;
-  password: string;
+          email: string;
+          password: string;
 };
 
 type SignInResponse = Promise<{
-  accessToken: string;
+          accessToken: string;
 }>;
 
 type SignUpRequest = {
-  email: string;
-  name: string;
-  password: string;
+          email: string;
+          name: string;
+          password: string;
 };
 
 type SignUpResponse = Promise<{
-  accessToken: string;
+          accessToken: string;
 }>;
 
 type MeRequest = {
-  accessToken: string;
+          accessToken: string;
 };
 
 type MeResponse = Promise<User>;
 
 class AuthApi {
-  async signIn(request: SignInRequest): SignInResponse {
-    const { email, password } = request;
+          async signIn(request: SignInRequest): SignInResponse {
+                    const { email, password } = request;
 
-    await wait(500);
+                    await wait(500);
 
-    return new Promise((resolve, reject) => {
-      try {
-        // Merge static users (data file) with persisted users (browser storage)
-        const mergedUsers = [...users, ...getPersistedUsers()];
+                    return new Promise((resolve, reject) => {
+                              try {
+                                        // Merge static users (data file) with persisted users (browser storage)
+                                        const mergedUsers = [...users, ...getPersistedUsers()];
 
-        // Find the user
-        const user = mergedUsers.find((user) => user.email === email);
+                                        // Find the user
+                                        const user = mergedUsers.find((user) => user.email === email);
 
-        if (!user || user.password !== password) {
-          reject(new Error('Please check your email and password'));
-          return;
-        }
+                                        if (!user || user.password !== password) {
+                                                  reject(new Error('Please check your email and password'));
+                                                  return;
+                                        }
 
-        // Create the access token
-        const accessToken = sign({ userId: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+                                        // Create the access token
+                                        const accessToken = sign({ userId: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
-        resolve({ accessToken });
-      } catch (err) {
-        console.error('[Auth Api]: ', err);
-        reject(new Error('Internal server error'));
-      }
-    });
-  }
+                                        resolve({ accessToken });
+                              } catch (err) {
+                                        console.error('[Auth Api]: ', err);
+                                        reject(new Error('Internal server error'));
+                              }
+                    });
+          }
 
-  async signUp(request: SignUpRequest): SignUpResponse {
-    const { email, name, password } = request;
+          async signUp(request: SignUpRequest): SignUpResponse {
+                    const { email, name, password } = request;
 
-    await wait(1000);
+                    await wait(1000);
 
-    return new Promise((resolve, reject) => {
-      try {
-        // Merge static users (data file) with persisted users (browser storage)
-        const mergedUsers = [...users, ...getPersistedUsers()];
+                    return new Promise((resolve, reject) => {
+                              try {
+                                        // Merge static users (data file) with persisted users (browser storage)
+                                        const mergedUsers = [...users, ...getPersistedUsers()];
 
-        // Check if a user already exists
-        let user = mergedUsers.find((user) => user.email === email);
+                                        // Check if a user already exists
+                                        let user = mergedUsers.find((user) => user.email === email);
 
-        if (user) {
-          reject(new Error('User already exists'));
-          return;
-        }
+                                        if (user) {
+                                                  reject(new Error('User already exists'));
+                                                  return;
+                                        }
 
-        user = {
-          id: createResourceId(),
-          avatar: undefined,
-          email,
-          name,
-          password,
-          plan: 'Standard',
-        };
+                                        user = {
+                                                  id: createResourceId(),
+                                                  avatar: undefined,
+                                                  email,
+                                                  name,
+                                                  password,
+                                                  plan: 'Standard',
+                                        };
 
-        persistUser(user);
+                                        persistUser(user);
 
-        const accessToken = sign({ userId: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+                                        const accessToken = sign({ userId: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
-        resolve({ accessToken });
-      } catch (err) {
-        console.error('[Auth Api]: ', err);
-        reject(new Error('Internal server error'));
-      }
-    });
-  }
+                                        resolve({ accessToken });
+                              } catch (err) {
+                                        console.error('[Auth Api]: ', err);
+                                        reject(new Error('Internal server error'));
+                              }
+                    });
+          }
 
-  me(request: MeRequest): MeResponse {
-    const { accessToken } = request;
+          me(request: MeRequest): MeResponse {
+                    const { accessToken } = request;
 
-    return new Promise((resolve, reject) => {
-      try {
-        // Decode access token
-        const decodedToken = decode(accessToken) as any;
+                    return new Promise((resolve, reject) => {
+                              try {
+                                        // Decode access token
+                                        const decodedToken = decode(accessToken) as any;
 
-        // Merge static users (data file) with persisted users (browser storage)
-        const mergedUsers = [...users, ...getPersistedUsers()];
+                                        // Merge static users (data file) with persisted users (browser storage)
+                                        const mergedUsers = [...users, ...getPersistedUsers()];
 
-        // Find the user
-        const { userId } = decodedToken;
-        const user = mergedUsers.find((user) => user.id === userId);
+                                        // Find the user
+                                        const { userId } = decodedToken;
+                                        const user = mergedUsers.find((user) => user.id === userId);
 
-        if (!user) {
-          reject(new Error('Invalid authorization token'));
-          return;
-        }
+                                        if (!user) {
+                                                  reject(new Error('Invalid authorization token'));
+                                                  return;
+                                        }
 
-        resolve({
-          id: user.id,
-          avatar: user.avatar,
-          email: user.email,
-          name: user.name,
-          plan: user.plan,
-        });
-      } catch (err) {
-        console.error('[Auth Api]: ', err);
-        reject(new Error('Internal server error'));
-      }
-    });
-  }
+                                        resolve({
+                                                  id: user.id,
+                                                  avatar: user.avatar,
+                                                  email: user.email,
+                                                  name: user.name,
+                                                  plan: user.plan,
+                                        });
+                              } catch (err) {
+                                        console.error('[Auth Api]: ', err);
+                                        reject(new Error('Internal server error'));
+                              }
+                    });
+          }
 }
 
 export const authApi = new AuthApi();
