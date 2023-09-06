7a700b32438263a69c42f2080fad453a0dc4495b
banexxl <damjanovic.branislav@gmail.com>
2023-09-06 21:52:53 +0200

    index gura na /dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 import { Seo } from 'src/components/seo';
 import { usePageView } from 'src/hooks/use-page-view';
 import { Layout } from 'src/layouts/dashboard';
+import { paths } from 'src/paths';
 
 const Page: NextPage = () => {
 
           usePageView();
+          const router = useRouter()
+          if (typeof window !== 'undefined') {
+                    router.replace(paths.dashboard.index); // Replace '/new-route' with your desired destination
+          }
 
           return (
                     <>
