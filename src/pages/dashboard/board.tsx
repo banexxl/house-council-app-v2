1f5bcdf446e2366514f1b6d7c8ddb365071129f5
banexxl <damjanovic.branislav@gmail.com>
2023-10-27 19:57:24 +0200

    pocetka boarda

--- src/pages/dashboard/board.tsx
+++ src/pages/dashboard/board.tsx
@@ -15,6 +15,7 @@ import { ColumnCard } from 'src/sections/dashboard/board/column-card';
 import { ColumnAdd } from 'src/sections/dashboard/board/column-add';
 import { useDispatch, useSelector } from 'src/store';
 import { thunks } from 'src/thunks/board';
+import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 
 const useColumnsIds = (): string[] => {
 
@@ -45,9 +46,21 @@ const Page: NextPage = () => {
           const [currentTaskId, setCurrentTaskId] = useState<string | null>(null);
 
           usePageView();
-
           useBoard();
+          const [age, setAge] = useState<string | number>('');
+          const [open, setOpen] = useState(false);
 
+          const handleChange = (event: SelectChangeEvent<typeof age>) => {
+                    setAge(event.target.value);
+          };
+
+          const handleClose = () => {
+                    setOpen(false);
+          };
+
+          const handleOpen = () => {
+                    setOpen(true);
+          };
           const handleDragEnd = useCallback(
                     async ({ source, destination, draggableId }: DropResult): Promise<void> => {
                               try {
@@ -193,8 +206,33 @@ const Page: NextPage = () => {
                                                   pt: 8,
                                         }}
                               >
-                                        <Box sx={{ px: 3 }}>
+                                        <Box sx={{ px: 3, display: 'flex', flexDirection: 'column' }}>
+
                                                   <Typography variant="h4">Board</Typography>
+
+                                                  <FormControl sx={{ m: 1, minWidth: 120 }}>
+                                                            <InputLabel id="demo-controlled-open-select-label">Age</InputLabel>
+                                                            <Select
+                                                                      labelId="demo-controlled-open-select-label"
+                                                                      id="demo-controlled-open-select"
+                                                                      open={open}
+                                                                      onClose={handleClose}
+                                                                      onOpen={handleOpen}
+                                                                      value={age}
+                                                                      label="Age"
+                                                                      onChange={handleChange}
+                                                                      sx={{ width: '400px' }}
+                                                            >
+                                                                      <MenuItem value="">
+                                                                                <em>None</em>
+                                                                      </MenuItem>
+                                                                      <MenuItem value={10}>Ten</MenuItem>
+                                                                      <MenuItem value={20}>Twenty</MenuItem>
+                                                                      <MenuItem value={30}>Thirty</MenuItem>
+                                                            </Select>
+                                                  </FormControl>
+
+
                                         </Box>
                                         <DragDropContext onDragEnd={handleDragEnd}>
                                                   <Box
@@ -237,6 +275,7 @@ const Page: NextPage = () => {
                     </>
           );
 };
+
 
 Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
