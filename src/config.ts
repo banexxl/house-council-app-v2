6a29bc625c965813e36c835450fd53266aab275b
banexxl <damjanovic.branislav@gmail.com>
2025-01-04 08:46:49 +0100

    fix(config): update Mapbox API key environment variable name

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const mapboxConfig = {
-     apiKey: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!,
+     apiKey: process.env.NEXT_PUBLIC_MAPBOX_API_KEY!,
 }
 
 export const enableDevTools = process.env.NODE_ENV === 'development';
