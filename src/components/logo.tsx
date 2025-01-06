ce2870f768b680dd7af08fa3fb13ca503359234c
banexxl <damjanovic.branislav@gmail.com>
2025-01-06 21:33:10 +0100

    feat(logo): redesign logo SVG with new building and window elements

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -13,15 +13,26 @@ export const Logo: FC = () => {
       width="100%"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        opacity={0.16}
-        d="M7.242 11.083c.449-1.674 2.17-3.394 3.843-3.843l10.434-2.796c1.673-.448 2.666.545 2.218 2.218L20.94 17.096c-.449 1.674-2.17 3.394-3.843 3.843L6.664 23.735c-1.673.448-2.666-.545-2.218-2.218l2.796-10.434Z"
+      {/* Building Base */}
+      <rect
+        x="4"
+        y="2"
+        width="16"
+        height="20"
         fill={fillColor}
       />
-      <path
-        d="M3.06 6.9c.448-1.674 2.168-3.394 3.842-3.843L17.336.261c1.673-.448 2.667.545 2.218 2.218l-2.796 10.434c-.449 1.674-2.169 3.394-3.843 3.843L2.481 19.552C.808 20-.185 19.007.263 17.334L3.06 6.9Z"
-        fill={fillColor}
-      />
+      {/* Windows */}
+      <rect x="6" y="6" width="3" height="3" fill="#FFFFFF" />
+      <rect x="11" y="6" width="3" height="3" fill="#FFFFFF" />
+      <rect x="16" y="6" width="3" height="3" fill="#FFFFFF" />
+      <rect x="6" y="11" width="3" height="3" fill="#FFFFFF" />
+      <rect x="11" y="11" width="3" height="3" fill="#FFFFFF" />
+      <rect x="16" y="11" width="3" height="3" fill="#FFFFFF" />
+      <rect x="6" y="16" width="3" height="3" fill="#FFFFFF" />
+      <rect x="11" y="16" width="3" height="3" fill="#FFFFFF" />
+      <rect x="16" y="16" width="3" height="3" fill="#FFFFFF" />
+      {/* Door */}
+      <rect x="10" y="16" width="4" height="5" fill="#FFFFFF" />
     </svg>
   );
 };
