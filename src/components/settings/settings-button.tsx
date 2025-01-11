5ad6673e4d0815ec2b2f68bcecb6f036f1f56fbd
banexxl <damjanovic.branislav@gmail.com>
2025-01-11 10:28:06 +0100

    feat(settings): disable settings button on specific URL and update styles

--- src/components/settings/settings-button.tsx
+++ src/components/settings/settings-button.tsx
@@ -5,41 +5,52 @@ import Box from '@mui/material/Box';
 import ButtonBase from '@mui/material/ButtonBase';
 import SvgIcon from '@mui/material/SvgIcon';
 import Tooltip from '@mui/material/Tooltip';
+import { usePathname } from 'next/navigation';
 
 interface SettingsButtonProps {
   onClick?: () => void;
 }
 
-export const SettingsButton: FC<SettingsButtonProps> = (props) => (
-  <Tooltip title="Settings">
-    <Box
-      sx={{
-        backgroundColor: 'background.paper',
-        borderRadius: '50%',
-        bottom: 0,
-        boxShadow: 16,
-        margin: (theme) => theme.spacing(4),
-        position: 'fixed',
-        right: 0,
-        zIndex: (theme) => theme.zIndex.speedDial,
-      }}
-      {...props}
-    >
-      <ButtonBase
+export const SettingsButton: FC<SettingsButtonProps> = (props) => {
+  const pathname = usePathname();
+
+  // Disable the button if on the specified URL
+  const isDisabled = pathname === '/dashboard/locations/add';
+
+  return (
+    <Tooltip title="Settings">
+      <Box
         sx={{
-          backgroundColor: 'primary.main',
+          backgroundColor: 'background.paper',
           borderRadius: '50%',
-          color: 'primary.contrastText',
-          p: '10px',
+          bottom: 0,
+          boxShadow: 16,
+          margin: (theme) => theme.spacing(4),
+          position: 'fixed',
+          right: 0,
+          zIndex: (theme) => theme.zIndex.speedDial,
         }}
       >
-        <SvgIcon>
-          <Settings03Icon />
-        </SvgIcon>
-      </ButtonBase>
-    </Box>
-  </Tooltip>
-);
+        <ButtonBase
+          sx={{
+            backgroundColor: isDisabled ? 'grey.500' : 'primary.main',
+            borderRadius: '50%',
+            color: isDisabled ? 'grey.300' : 'primary.contrastText',
+            p: '10px',
+            cursor: isDisabled ? 'not-allowed' : 'pointer',
+            pointerEvents: isDisabled ? 'none' : 'auto',
+          }}
+          disabled={isDisabled}
+          onClick={!isDisabled ? props.onClick : undefined}
+        >
+          <SvgIcon>
+            <Settings03Icon />
+          </SvgIcon>
+        </ButtonBase>
+      </Box>
+    </Tooltip>
+  );
+};
 
 SettingsButton.propTypes = {
   onClick: PropTypes.func,
