4a3c290997a39cfc7042f41d7dce427c17f85a82
banexxl <damjanovic.branislav@gmail.com>
2025-05-06 07:34:03 +0200

    feat: implement updateFeature function for managing feature updates

---
+++ src/app/actions/feature/feature-actions.ts
@@ -0,0 +1,42 @@
+'use server'
+
+import { revalidatePath } from "next/cache";
+import { supabase } from "src/libs/supabase/client";
+import { Feature } from "src/types/base-entity";
+
+export const updateFeature = async (id: string, feature: Partial<Feature>): Promise<{ success: boolean; updatedFeature?: Feature; error?: any }> => {
+     if (!feature || Object.keys(feature).length === 0) {
+          return { success: false, error: 'clients.clientSettingsNoEntityError' };
+     }
+
+     if (feature.name?.trim() === '') {
+          return { success: false, error: 'clients.clientSettingsNoNameError' };
+     }
+
+     if (feature.name) {
+          const { data: existingFeature } = await supabase
+               .from('tblFeatures')
+               .select('id')
+               .ilike('name', feature.name.trim())
+               .neq('id', id)
+               .single();
+
+          if (existingFeature) {
+               return { success: false, error: 'clients.clientSettingsAlreadyExists' };
+          }
+     }
+
+     const { data, error } = await supabase
+          .from('tblFeatures')
+          .update(feature)
+          .eq('id', id)
+          .select()
+          .single();
+
+     if (error) {
+          return { success: false, error };
+     }
+
+     revalidatePath('/dashboard/subscriptions');
+     return { success: true, updatedFeature: data };
+};
