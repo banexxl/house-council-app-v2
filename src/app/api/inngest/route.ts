ce8785307ccf80cd179fc1486206a9f85b057f73
banexxl <damjanovic.branislav@gmail.com>
2025-07-04 17:29:42 +0200

    feat: add inngest package and update API route to use serve function

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
-import { serve } from "inngest/next";
 import { inngest } from "src/inngest/client";
 import { checkIfLocationIsOccupied } from "src/inngest/functions/location-check";
+import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
