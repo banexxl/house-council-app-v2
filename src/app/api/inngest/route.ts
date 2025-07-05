1ed44833896690f6ce83d92bf0e333b35391bae2
banexxl <damjanovic.branislav@gmail.com>
2025-07-05 23:09:13 +0200

    fix: reorder import statements for consistency in API route

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { inngest } from "src/inngest/client";
-import { checkIfLocationIsOccupied } from "src/inngest/functions/location-check";
 import { serve } from "inngest/next";
+import { checkIfLocationIsOccupied } from "src/inngest/functions/location-check";
 import { checkAllClientSubscriptions } from "src/inngest/functions/check-free-trials";
 
 // Create an API that serves zero functions
