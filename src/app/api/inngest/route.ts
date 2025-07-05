6435e353e517df1c1b5b5919dacab7eba67ed00d
banexxl <damjanovic.branislav@gmail.com>
2025-07-05 22:24:38 +0200

    feat: add checkAllClientSubscriptions function to API route

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,13 @@
 import { inngest } from "src/inngest/client";
 import { checkIfLocationIsOccupied } from "src/inngest/functions/location-check";
 import { serve } from "inngest/next";
+import { checkAllClientSubscriptions } from "src/inngest/functions/check-free-trials";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
      client: inngest,
      functions: [
-          checkIfLocationIsOccupied
+          checkIfLocationIsOccupied,
+          checkAllClientSubscriptions
      ],
 });
