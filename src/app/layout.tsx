d284c0b8b6c7c5cd78b5400a01ee1a5acdec0fa6
Branislav <damjanovic.branislav@gmail.com>
2025-09-05 23:27:14 +0200

    refactor: move realtime subscriptions to dashboard layout and clean up unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import { useEffect, type ReactNode } from 'react';
+import { type ReactNode } from 'react';
 import { NProgress } from 'src/components/nprogress';
 import { Layout as RootLayout } from 'src/layouts/root';
-import { initAnnouncementsRealtime } from 'src/realtime/sb-realtime';
 import 'src/global.css';
 
 
@@ -15,29 +14,7 @@ const Layout = (props: LayoutProps) => {
 
   const { children } = props;
 
-  useEffect(() => {
-    let unsubscribe: (() => Promise<void>) | undefined;
-
-    const init = () => {
-      try {
-        unsubscribe = initAnnouncementsRealtime((payload) => {
-          payload.table;
-        });
-      } catch (err) {
-        console.error('[announcements] realtime init failed', err);
-      }
-    };
-
-    init();
-
-    return () => {
-      if (unsubscribe) {
-        unsubscribe()
-          .then(() => console.log('[announcements] realtime unsubscribed'))
-          .catch((err) => console.error('[announcements] unsubscribe failed', err));
-      }
-    };
-  }, []);
+  // Realtime subscriptions moved to dashboard layout so auth pages don't subscribe.
 
   return (
     <html>
