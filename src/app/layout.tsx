8685b2b6f843115de03a83838f9b00b1b13d79aa
banexxl <banexxl>
2025-11-26 02:25:15 +0100

    fix: update import path in next-env.d.ts and comment out unused AuthProvider in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import { type ReactNode } from 'react';
 import { NProgress } from 'src/components/nprogress';
 import { Layout as RootLayout } from 'src/layouts/root';
 import AuthProvider from 'src/contexts/auth/auth-provider';
 import { getViewer } from 'src/libs/supabase/server-auth';
 import ClientSubscriptionWatcher from 'src/realtime/client-subscription-watcher';
 
+
 export default async function Layout({ children }: { children: React.ReactNode }) {
 
-  const viewer = await getViewer();
+  // const viewer = await getViewer();
   // Realtime subscriptions moved to dashboard layout so auth pages don't subscribe.
 
   return (
     <html>
       <body>
-        <AuthProvider initialViewer={viewer}>
-          <RootLayout >
-            {children}
-            {/* Client-side realtime watcher: logout on non-active subscription */}
-            <ClientSubscriptionWatcher />
-            <NProgress />
-          </RootLayout>
-        </AuthProvider>
+        {/* <AuthProvider initialViewer={viewer}> */}
+        <RootLayout >
+          {children}
+          {/* Client-side realtime watcher: logout on non-active subscription */}
+          <ClientSubscriptionWatcher />
+          <NProgress />
+        </RootLayout>
+        {/* </AuthProvider> */}
       </body>
     </html >
   );
