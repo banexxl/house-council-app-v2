cc447a7409c4a8227718577162843295708b98d7
banexxl <banexxl>
2025-11-26 04:08:03 +0100

    fix: add router import and update session refresh logic in useSessionUpdater

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,6 +10,7 @@ import { DefaultPageSkeleton } from "src/sections/dashboard/skeletons/default-pa
 const Page = async () => {
 
   const { client, tenant, admin, clientMember } = await getViewer();
+  console.log(client);
 
   if (!client && !tenant && !admin && !clientMember) {
     await logout();
