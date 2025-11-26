eec70183b8be16450c74758ec19c9b007e29b025
banexxl <banexxl>
2025-11-26 03:02:59 +0100

    fix: update package.json scripts for turbopack support and enhance dashboard layout with suspense

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,11 @@
-'use client';
+import { Suspense, type ReactNode } from 'react';
+import { DefaultPageSkeleton } from 'src/sections/dashboard/skeletons/default-page-skeleton';
+import { Layout as DashboardLayout } from 'src/layouts/components/dashboard';
 
-export { Layout as default } from 'src/layouts/components/dashboard';
+export default function Layout({ children }: { children: ReactNode }) {
+     return (
+          <Suspense fallback={<DefaultPageSkeleton />}>
+               <DashboardLayout>{children}</DashboardLayout>
+          </Suspense>
+     );
+}
